dd0a6fc47b86b95a28b85730eb128bb2ce832cc6
liquancool <llei0111@bths.edu>
2022-12-06 18:10:23 -0500

    Did javadoc

--- src/Time.java
+++ src/Time.java
@@ -1,8 +1,17 @@
+/**
+ * The Time class represents the time. The time is recorded in hours, minutes, and seconds.
+ */
 public class Time {
     private int hours;
     private int minutes;
     private int seconds;
 
+    /**
+     * Constructor for the Time class. This creates a new instance of Time given the parameters.
+     * @param h represents the hours
+     * @param m represents the minutes
+     * @param s represents the seconds
+     */
     public Time(int h, int m , int s)
     {
         hours = h;
@@ -10,21 +19,35 @@ public class Time {
         seconds = s;
     }
 
+    /**
+     * The getSeconds method will return the seconds
+     * @return returns seconds as an integer
+     */
     public int getSeconds()
     {
         return seconds;
     }
-
+    /**
+     * The getMinutes method will return the minutes
+     * @return returns minutes as an integer
+     */
     public int getMinutes()
     {
         return minutes;
     }
-
+    /**
+     * The getHours method will return the hours
+     * @return returns hours as an integer
+     */
     public int getHours()
     {
         return hours;
     }
 
+    /**
+     * The tick method will add 1 second to the time which represents 1 second passing, it will also
+     * increase or set to zero minutes and hours if it goes above the standard limit.
+     */
     public void tick()
     {
         seconds++;
@@ -44,6 +67,11 @@ public class Time {
         }
     }
 
+    /**
+     * The add method will add two times together by adding each specific variable to each other, it will also
+     *      * increase or set to zero minutes and hours if it goes above the standard limit.
+     * @param t
+     */
     public void add(Time t)
     {
         seconds+=t.getSeconds();
@@ -65,6 +93,10 @@ public class Time {
         }
     }
 
+    /**
+     * The toString method will return a string with the time, padded with 1 zero if a value is single digit and separated by colons
+     * @return
+     */
     public String toString()
     {
         return String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
